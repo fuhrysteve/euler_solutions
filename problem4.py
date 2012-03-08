4efcf93e6673f45e7214fb5d14173963b17ed3d1
Stephen J. Fuhry <steve@montmere.com>
2012-03-08 18:46:57 -0500

    problem 4

---
+++ problem4.py
@@ -0,0 +1,21 @@
+# largest palendrome that is a product of 2x 3 digit numbers
+
+lpal = 0
+facs = []
+
+def is_a_pal(n):
+   l = list("%d" % (n))
+   return l[::-1] == l
+
+for i in xrange(100,999):
+   for j in xrange(100,999):
+      k = i * j
+      if is_a_pal(k) and k > lpal:
+         facs = [i,j]
+         lpal = k
+
+print facs
+print lpal
+   
+
+
